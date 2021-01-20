1b495c89886c215b2d4d8c24d937217b4be28e03
Scarcrux <reg@scarcrux.com>
2021-01-19 22:09:46 -0500

    Pass reducer tests - #1
    Note that the test component doesn't automatically refresh data on delete

--- src/containers/Campuses.js
+++ src/containers/Campuses.js
@@ -2,10 +2,33 @@
 
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { addCampus, removeCampus, updateCampus } from '../actions/campuses'
 
 class Campuses extends Component {
   constructor(props) {
     super(props);
+  }
+
+  handleSubmit = () => {
+    const updateTest = this.props.campuses[1];
+    updateTest.name = "Test College";
+    //console.log(updateTest)
+    this.props.updateCampus(updateTest);
+  }
+
+  handleRemove = () => {
+    const temp = this.props.campuses[20]
+    this.props.removeCampus(temp);
+  }
+
+  handleNewSubmit = () => {
+    const updateTest = {
+      "name" : "Test College",
+      "imageURL" : "http://placeimg.com/640/480/cats",
+      "address" : "1143 Nuvem Ter, East Karleyport, ND 44998",
+      "description" : "Velit neque provident itaque. Quisquam vitae unde consequatur itaque facilis perspiciatis eum consequatur. Non totam hic quaerat rerum. Sit tempore doloremque nesciunt quis praesentium et asperiores. Voluptatum odio corrupti aut harum ipsum ut aperiam sunt illum. Maiores autem consectetur aut amet possimus doloribus est. Ducimus magnam impedit sed modi illum iste quo voluptatum. Laborum quos qui. Fuga id et dolor enim saepe qui voluptatem inventore animi. Ut voluptates sint."
+   }
+    this.props.addCampus(updateTest);
   }
 
   render () {
@@ -13,10 +36,14 @@ class Campuses extends Component {
     const campusItemArr = campuses.map(campus => (
       <li key={campus.id}>{campus.name} - {campus.imageUrl} - {campus.address} - {campus.description}</li>
     ));
+
     return (
       <div className="container">
         <div className="campus-list">
           <ul>{campusItemArr}</ul>
+          <div><button onClick={this.handleSubmit}>Test Update</button></div>
+          <div><button onClick={this.handleNewSubmit}>Test Create</button></div>
+          <div><button onClick={this.handleRemove}>Test Delete</button></div>
         </div>
       </div>
     );
@@ -27,7 +54,11 @@ const mapStateToProps = (state) => ({
   campuses: state.campuses
 });
 
-const mapDispatchToProps = dispatch => ({});
+const mapDispatchToProps = dispatch => ({
+  addCampus: (campus) => dispatch(addCampus(campus)),
+  removeCampus: (campus) => dispatch(removeCampus(campus)),
+  updateCampus: (campus) => dispatch(updateCampus(campus)),
+});
 
 const connector = connect(mapStateToProps, mapDispatchToProps);
 export default connector(Campuses);
